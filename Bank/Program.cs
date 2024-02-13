56a66817f92054f001e8760889d18a1f54b5f1a6
antoinel74 <antoine.lansman@gmail.com>
2024-02-13 16:02:56 +0100

    add authentification

--- Bank/Program.cs
+++ Bank/Program.cs
@@ -57,43 +57,54 @@ namespace BankProgram {
     class Program {
         static void Main(string[] args)
         {
+            string usr = "antoine";
+            string pswrd = "1111";
+
             Console.WriteLine("Welcome to SpyBank");
-            Console.Write("Please enter your name :");
-            string name = Console.ReadLine()!;
-            Client client = new Client(1, name, DateTime.Now);
-            BankAccount bankAccount = new BankAccount(client, 1000, "Standard");
+            Console.Write("Please enter your username :");
+            string enteredUsername = Console.ReadLine()!;
 
-            // USER DASHBOARD  
-            Console.WriteLine($"Welcome, {client.Name}!");
-            Console.WriteLine("What would you like to do?");
-            Console.WriteLine("1. Check Balance");
-            Console.WriteLine("2. Withdraw");
-            Console.WriteLine("3. Deposit");
-            Console.Write("Enter your choice (1, 2, or 3): ");
+           Console.Write("Enter password :");
+           string enteredPswrd = Console.ReadLine()!;
 
-            string choice = Console.ReadLine()!;
+           if (enteredUsername == usr && enteredPswrd == pswrd)
+           {
+                Client client = new Client(1, usr, DateTime.Now);
+                BankAccount bankAccount = new BankAccount(client, 1000, "Standard");
 
-            switch (choice)
-            {
-                case "1":
-                    Console.WriteLine($"Current balance : {bankAccount.CheckBalance()}");
-                    break;
-                case "2":
-                    Console.Write("Enter the amount you want to withdraw: ");
-                    double amount = Convert.ToDouble(Console.ReadLine());
-                    bankAccount.Withdraw(amount);
-                    Console.WriteLine($"Succes. Current balance: {bankAccount.CheckBalance()}");
-                    break;
-                case "3":
-                    Console.Write("Enter the amount you want to deposit: ");
-                    double deposit = Convert.ToDouble(Console.ReadLine());
-                    bankAccount.Deposit(deposit);
-                    Console.WriteLine($"Succes. Current balance: {bankAccount.CheckBalance()}");
-                    break;
-                default:
-                    Console.WriteLine("Invalid. Type 1, 2 or 3.");
-                    break;
-            }
+                // USER DASHBOARD  
+                Console.WriteLine($"Welcome, {client.Name}!");
+                Console.WriteLine("What would you like to do?");
+                Console.WriteLine("1. Check Balance");
+                Console.WriteLine("2. Withdraw");
+                Console.WriteLine("3. Deposit");
+                Console.Write("Enter your choice (1, 2, or 3): ");
+
+                string choice = Console.ReadLine()!;
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.WriteLine($"Current balance : {bankAccount.CheckBalance()}");
+                        break;
+                    case "2":
+                        Console.Write("Enter the amount you want to withdraw: ");
+                        double amount = Convert.ToDouble(Console.ReadLine());
+                        bankAccount.Withdraw(amount);
+                        Console.WriteLine($"Succes. Current balance: {bankAccount.CheckBalance()}");
+                        break;
+                    case "3":
+                        Console.Write("Enter the amount you want to deposit: ");
+                        double deposit = Convert.ToDouble(Console.ReadLine());
+                        bankAccount.Deposit(deposit);
+                        Console.WriteLine($"Succes. Current balance: {bankAccount.CheckBalance()}");
+                        break;
+                    default:
+                        Console.WriteLine("Invalid. Type 1, 2 or 3.");
+                        break;
+                }
+           }
+           Console.WriteLine("Invalid username or password. Access denied.");
         }
     }
 }
